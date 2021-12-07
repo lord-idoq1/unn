376d8f87c7e15b9b05be7dba80bde54e2080a639
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2021-12-07 09:16:16 +0700

    Create Ini.js

---
+++ Ini.js
@@ -0,0 +1,5 @@
+case 'ido':
+                                         ojan = fetchJson('https://api-lpg.herokuapp.com/other/pornoo')
+                                         ido = getBuffer(ojan.result.video
+                                         client.sendMessage(from, ido, video, {quoted: mek, caption: `Link: ${ojan.result.source}``})
+                                         break
